15d2ef66ada75dd246318113ed4e3f57cf99b887
QQruz <bbojan@protonmail.com>
2019-11-17 13:15:16 +0100

    Add nav

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -33,7 +33,25 @@
                 <div class="collapse navbar-collapse" id="navbarSupportedContent">
                     <!-- Left Side Of Navbar -->
                     <ul class="navbar-nav mr-auto">
-
+                        <li class="nav-item">
+                            <a class="nav-link" href="{{ route('jobs.index') }}">Home</a>
+                        </li>
+                        @can('post-jobs')
+                        <li class="nav-item">
+                            <a class="nav-link" href="{{ route('jobs.new') }}">New job</a>
+                        </li>
+                        @endcan
+                        @can('edit-users')
+                        <li class="nav-item dropdown">
+                            <a id="adminDropdown" class="nav-link dropdown-toggle" href="#" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+                                Admin <span class="caret"></span>
+                            </a>
+                            <div class="dropdown-menu dropdown-menu-right" aria-labelledby="adminDropdown">
+                                <a class="dropdown-item" href="{{ route('users.notApproved') }}">New users</a>
+                                <a class="dropdown-item" href="{{ route('users.listTrashed') }}">Spam</a>
+                                </div>
+                            </li>
+                        @endcan
                     </ul>
 
                     <!-- Right Side Of Navbar -->
